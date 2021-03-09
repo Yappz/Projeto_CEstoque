a16fc6db65048f877065c310796d433ebe370ed3
Yappz <douglas.torres1997@gmail.com>
2021-03-09 15:47:16 -0300

    first commit

---
+++ src/controlers/authcontroler.js
@@ -0,0 +1,27 @@
+/* Controler de autentificação*/
+
+const express = require ('express');
+
+const User = require ('../models/user');
+
+/* Sera feito uma busca do express a classe router para definir a rota so para usuários */
+
+const router = express.Router();
+
+/* Assim que o usuário chamar essa rota, um usuário sera criado inserindo todos os paramentros em re.body
+retornando algum erro caso haja algum erro no cadastro de novo usuário. */
+
+router.post('/register' , async (req, res) => {
+ 
+    try{
+    const user = await User.create(req.body);
+    
+    return res.send ({ user });
+    }catch (err) {
+        return res.status(400).send({error: 'Registration failed'});
+    }
+} );
+
+/* Todas as rotas definidas aki serão endereçadas ao /auth */
+
+module.exports = app => app.use('/auth', router);
